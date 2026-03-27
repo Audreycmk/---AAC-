f001744e430161c26e742ed8d34b7b23cb1e2617
Audrey Chung <audreycmk@gmail.com>
2026-03-27 03:04:03 +0000

    Arrange fro twt2026

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/Audrey/Desktop/Desktop/---AAC-/app/page.tsx
+// File: /workspaces/---AAC-/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
